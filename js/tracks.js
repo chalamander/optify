5db2cd778ad68145e3a4f298a5589a6105ffef93
Sailesh Patel <saileshapatel@gmail.com>
2017-04-22 20:57:48 +0100

    rid of argument from getElement

--- js/tracks.js
+++ js/tracks.js
@@ -1,10 +1,10 @@
-function getElement(obj, key, element, place){
+function getElement(obj, key, place){
   for(var key in obj){
     if(obj.hasOwnProperty(key)){
       var value = obj[key];
       console.log(value.name);
 
-      var button = document.createElement(element);
+      var button = document.createElement('button');
       button.innerHTML = "<a href='https://open.spotify.com/track'" + value.url + "'>" + value.name + "";
       var placeEl = document.getElementById(place)
       placeEl.append(button);
