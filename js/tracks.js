a8f57c33a5c8fd50c41843cb0f09896d4cb8f11f
Sailesh Patel <saileshapatel@gmail.com>
2017-04-22 20:42:22 +0100

    buttons generate

--- js/tracks.js
+++ js/tracks.js
@@ -1,8 +1,13 @@
-function getElement(obj, key){
+function getElement(obj, key, element, place){
   for(var key in obj){
     if(obj.hasOwnProperty(key)){
       var value = obj[key];
       console.log(value.name);
+
+      var button = document.createElement(element);
+      button.innerHTML = value.name;
+      var placeEl = document.getElementById(place)
+      placeEl.append(button);
     }
   }
 }
