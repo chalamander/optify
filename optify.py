d18baf016dfb15ce6ebfc751b8b9cbf94cc59eed
Chalamander <charliepostgate@hotmail.co.uk>
2017-04-23 01:29:53 +0100

    improved algorithm and improved console output for testing

--- optify.py
+++ optify.py
@@ -16,11 +16,13 @@ params = urllib.urlencode({
 
 def getEmotionJson():
     conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{ 'url': 'http://www.islamiclife.com/2014/12/11/4824.jpeg' }", headers)
+    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{ 'url': 'http://www.pngall.com/wp-content/uploads/2016/04/Happy-Person-Free-Download-PNG.png' }", headers)
     response = conn.getresponse()
     data = response.read().decode('utf-8')
     json_obj = json.loads(data)
     result = json_obj[0]
     result = result['scores']
     conn.close()
+    print("PHOTO VALS")
+    print(json.dumps(result))
     return json.dumps(result)
--- read-playlist.py
+++ read-playlist.py
@@ -3,8 +3,7 @@ import spotipy
 import json
 import pprint
 import requests
-from emotionsToFeatures import getFeatureJson
-
+from optify import getEmotionJson
 
 #Initialise Spotify object
 client_credentials_manager = SpotifyClientCredentials()
@@ -12,9 +11,10 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 #spotify playlist URI to use
 #uri = 'spotify:user:spotifycharts:playlist:37i9dQZEVXbJiZcmkrIHGU' #charts
-uri = 'spotify:user:flappster2:playlist:1pMn56LnnoiRXEi0ISTysa' #musica
+#uri = 'spotify:user:flappster2:playlist:1pMn56LnnoiRXEi0ISTysa' #musica
 #uri = 'spotify:user:funnybunny000000:playlist:4EoPt05ztUjVaujcWbUL2Z' #sadness
 #uri = 'spotify:user:spotify:playlist:65V6djkcVRyOStLd8nza8E' #happy
+uri = 'spotify:user:sanik007:playlist:4AnAUkQNrLKlJCInZGSXRO' #happy2
 
 username = uri.split(':')[2]
 playlist_id = uri.split(':')[4]
@@ -23,6 +23,8 @@ playlist_id = uri.split(':')[4]
 results = sp.user_playlist(username, playlist_id)
 #r = requests.get('https://api.spotify.com/v1/users/{user_id}/playlists/' + playlist_id)
 #print r.text
+
+print 'Calculating....'
 
 #Get URI and key audio features of all tracks in playlist
 tracks = []
@@ -39,42 +41,61 @@ for i, t in enumerate(results['tracks']['items']):
     })
     #print json.dumps(tracks[i], indent=4)
 
-#Get JSON object with image attribute values
-img_values = json.loads(getFeatureJson())
+#get image emotion values
+img_values = json.loads(getEmotionJson())
 
 #happy json
-img_dance = float(img_values["danceability"])
-img_energy = float(img_values['energy'])
-img_valence = float(img_values['valence'])
-img_tempo = float(img_values['tempo'])
-img_mode = float(img_values['mode'])
+img_sadness = float(img_values["sadness"])
+img_happiness = float(img_values['happiness'])
+img_anger = float(img_values['anger'])
+img_surprise = float(img_values['surprise'])
+img_fear = float(img_values['fear'])
 
 filtered_tracks = []
 
 for x in range(0, len(tracks)):
-    valid = True
+    appropriate = 0
     dance = tracks[x]['features']['dance']
     energy = tracks[x]['features']['energy']
-    valence = tracks[x]['features']['mode']
-    tempo = tracks[x]['features']['valence']
-    mode = tracks[x]['features']['tempo']
-    #if danceability in 25% range
-    #if not (dance-0.5 <= img_dance <= dance+0.5):
-    #    valid = False
-    if not (img_energy-0.2 <= energy <= img_energy+0.2):
-        valid = False
-    #if not (img_valence-0.2 <= valence <= img_valence+0.2):
-    #    valid = False
-    if not (tempo-50 <= img_tempo <= tempo+50):
-        valid = False
-    #if not (mode == img_mode):
-    #    valid = False
+    valence = tracks[x]['features']['valence']
+    tempo = tracks[x]['features']['tempo']
+    mode = tracks[x]['features']['mode']
 
-    print "energy min: ", (energy - 0.2), " energy max: " , (energy + 0.27)
-    #print "valence min: ", (valence - 0.2), " valence max: ", (valence + 0.27)
+    appropriate_criteria = ""
 
-    if valid:
+    #if happy and high energy
+    if (img_happiness > 0.5 or img_anger > 0.6) and (energy > 0.7):
+        appropriate += 1
+        appropriate_criteria += " high energy(" + str(energy) + ")"
+    #if not happy or sad or high fear, and energy is low
+    if ((img_happiness < 0.5 or img_sadness > 0.5 or img_fear > 0.5) and (img_anger < 0.4)) and (energy  < 0.5):
+        appropriate += 1
+        appropriate_criteria += " low energy(" + str(energy) + ")"
+    #if high happiness or high anger, and mode = 1
+    if (img_happiness > 0.6 or img_anger > 0.6) and (mode > 0.5):
+        appropriate += 1
+        appropriate_criteria += " major key(" + str(mode) + ")"
+    #if low happiness or high sadness or low anger, and mode = 0
+    if ((img_happiness < 0.4 or img_sadness > 0.6) or img_anger < 0.4) and (mode < 0.5):
+        appropriate += 1
+        appropriate_criteria += " minor key(" + str(mode) + ")"
+    #if low happiness or high sadness and low valence
+    if ((img_happiness < 0.5 or img_sadness > 0.5) and img_anger < 0.5 and valence < 0.35):
+        appropriate += 1
+        appropriate_criteria += " low valence(" + str(valence) + ")"
+    #if high happiness and high surprise, and danceability > 0.7
+    if (img_happiness > 0.6 or img_surprise > 0.6) and dance > 0.7:
+        appropriate += 1
+        appropriate_criteria += " high danceability(" + str(dance) + ")"
+    #if high anger or high surprise and tempo > 120
+    if (img_anger > 0.6 or img_surprise > 0.6) and tempo > 120:
+        appropriate +=1
+        appropriate_criteria += " high tempo(" + str(tempo) + ")"
+
+    if appropriate > 2:
         filtered_tracks.append(tracks[x]['uri'])
+        print (sp.track(tracks[x]['uri'])['name'], ": ", appropriate_criteria)
+
 
 print filtered_tracks
 
