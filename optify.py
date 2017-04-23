260a5e8ddffff19f0f2912e607265e918d0b4aa0
Jenny <myersj@aston.ac.uk>
2017-04-23 10:22:42 +0100

    fixed merges better *thumbs up*, now it actually works again

--- optify.py
+++ optify.py
@@ -15,21 +15,16 @@ def getEmotionJson(body):
     conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
     #print (body)
     conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{ 'url': '" + body + "'}" , headers)
-
+    response = conn.getresponse()
 #hap http://www.pngall.com/wp-content/uploads/2016/04/Happy-Person-Free-Download-PNG.png
 #sad http://cliparts.co/cliparts/di9/rad/di9radxAT.jpg
 #angry http://www.islamiclife.com/2014/12/11/4824.jpeg
-
-def getEmotionJson():
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, "{ 'url': 'http://www.pngall.com/wp-content/uploads/2016/04/Happy-Person-Free-Download-PNG.png' }", headers)
-    response = conn.getresponse()
     data = response.read().decode('utf-8')
     json_obj = json.loads(data)
     #print(json_obj)
     result = json_obj[0]
     result = result['scores']
-    #print(result)
+    print(result)
     conn.close()
     #print("PHOTO VALS")
     #print(json.dumps(result))
